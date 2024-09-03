2f5d3de3202450fb94143dfa9e473814d7c80f67
km. <84655311+km-hagana@users.noreply.github.com>
2024-09-03 17:48:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function btnGo() {
   textarea.focus();
   // PC版クエリ始まりeyJ0e ~ 終わり9fQ fX0 n19 まだある？集める　ケタが決まってないから法則性でマッチさせる
   // 正規表現   0:nico, 1:sm, 2:|&nico|, 3:sm
-  text = text.replace(/(?:(?:https?:\/\/)?(?:nico\.ms|(?:(?:www|sp)?\.)?nico(?:video|chart)\.jp)?\/)?(?:watch\/)?((?:[a-z]{2})[0-9]+|[0-9]{10,})(?:[\?\&][a-z_]+=(?:(?:[a-zA-Z0-9_\-]+|eyJ0e[a-zA-Z]+(?:9fQ|fX0|n19))(?:https?)?))*|((?:\|)?\&nicovideo\((?:[a-z]{2})?[0-9]+\)(?:\|)?)/g , 
+  text = text.replace(/(?:(?:https?:\/\/)?(?:nico\.ms|(?:(?:www|sp)?\.)?nico(?:video|chart)\.jp)?\/)?(?:watch\/)?((?:sm|nm|so)[0-9]+|[0-9]{10,})(?:[\?\&][a-z_]+=(?:(?:[a-zA-Z0-9_\-]+|eyJ0e[a-zA-Z]+(?:9fQ|fX0|n19))(?:https?)?))*|((?:\|)?\&nicovideo\((?:[a-z]{2})?[0-9]+\)(?:\|)?)/g , 
     function(){     // 特定の文字に置換するのではなくfunctionで条件付け
       // ニコニコにマッチするが、3つ目のカッコである|&nicovideo|形式は変換しない
       if(!arguments[2]) {
