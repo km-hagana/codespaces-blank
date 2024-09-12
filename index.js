ba3b0deae9a886e3b08f45b47f95a2a07cf8f0dc
km. <84655311+km-hagana@users.noreply.github.com>
2024-09-12 20:43:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ function btnGo() {
   // テキストの内容を取得
   var text = textarea.value;
 
+  /// サムネ欄 (1回でいいからループ外で設定しとく)
+    //画像を表示するプレビュー欄取得
+    const pre = document.querySelector('#previewColumn');
+    // 説明文を消す
+    pre.innerHTML = "";
+    // 左寄せにする
+    pre.style.textAlign = "left";
+
   // matchは第2引数なし  gはグローバル(全文)検索
   // []内でのエスケープは[]内で意味を持つものに付ける→ [ ] - ^ $
 
@@ -37,12 +45,6 @@ function btnGo() {
     + "\\&youtube\\(https?:\\/\\/www\\.youtube\\.com\\/watch\\?v=[a-zA-Z0-9_\\-]{11}\\)(?:\\{[0-9]{2,4}\\,[0-9]{2,4}\\})?"
     , "g")
   );
- 
-  /// サムネ欄 (1回でいいからループ外で設定しとく)
-    //画像を表示するプレビュー欄取得
-    const pre = document.querySelector('#previewColumn');
-    // 説明文を消す
-    pre.innerHTML = "";
 
   try{ // try...catch文 : length null(テキストない時)エラー回避
     for( let i=0; i < match.length; i++){
@@ -54,25 +56,29 @@ function btnGo() {
       if( nicoID && !ytID ){
         let nicoStr = "\&nicovideo\(" + nicoID + "\)";
         inBoxStr.push(nicoStr);
-          // サムネ
       }else if( ytID && !nicoID ){
         let ytStr = "\&youtube\(https:\/\/www\.youtube\.com\/watch\?v=" + ytID + "\)\{342\,187\}";
         inBoxStr.push(ytStr);
       }
 
-      ///サムネ表示
-        /// ニコニコ
-        if( nicoID && !ytID ){
-        let thumbnail = document.createElement("img");
-        thumbnail.src = "https:\/\/nicovideo\.cdn\.nimg\.jp\/thumbnails\/" + nicoID + "\/" + nicoID + "\.12345678"; //←8ケタのランダム数字が付く
-        // プレビュー欄の子要素として最後に追加していく
-        pre.appendChild(thumbnail);
-      /// ようつべ
-      } else if( ytID  && !nicoID) {
-        let thumbnail = document.createElement("img");
-        thumbnail.src = "https:\/\/img\.youtube\.com\/vi\/" + ytID + "\/default\.jpg";
-        pre.appendChild(thumbnail);
-      }
+  ///サムネ表示
+    /// ニコニコ
+    if( nicoID && !ytID ){
+      // 動画情報
+      let thumbnail = document.createElement("iframe");
+      // サムネ let thumbnail = document.createElement("img");
+      thumbnail.src = "https:\/\/ext\.nicovideo\.jp\/thumb\/" + nicoID;
+      thumbnail.scrolling="no";
+      // サムネ thumbnail.src = "https:\/\/nicovideo\.cdn\.nimg\.jp\/thumbnails\/" + nicoID + "\/" + nicoID + "\.12345678"; //←8ケタのランダム数字が付く
+      // プレビュー欄の子要素として最後に追加していく
+      pre.appendChild(thumbnail);
+    /// ようつべ
+    } else if( ytID  && !nicoID) {
+      let thumbnail = document.createElement("img");
+      thumbnail.src = "https:\/\/img\.youtube\.com\/vi\/" + ytID + "\/mqdefault\.jpg";
+      pre.appendChild(thumbnail);
+    }
+
     } //for終わり
   } catch (err) {return;}
 
